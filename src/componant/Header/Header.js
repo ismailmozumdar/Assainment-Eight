120504a1db79c719dc6f6fda20c4201be5dd7b05
ismailmozumdar <ismailmozumdar770@gmail.com>
2022-03-26 17:25:41 +0600

    this assainment is redy to submit

--- src/componant/Header/Header.js
+++ src/componant/Header/Header.js
@@ -3,26 +3,10 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container">
-    <a className="navbar-brand" href="./">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="./">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="./">Link</a>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+           <nav className=" bg-light">
+  <div className="container App">
+    <h1>BOOK CORNER</h1>
+    <h4>24 hours service</h4>
   </div>
 </nav>
         </div>
